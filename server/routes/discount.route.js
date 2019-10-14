726dd2d672939706ea692983c7a2be24f4401270
tuananhgk <anhtuangk98@gmail.com>
2019-10-14 14:46:13 +0700

    up

--- server/routes/discount.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var discountCodeController = require('../controllers/discountCode.controller');
-var cors = require('cors');
-router.options('/', cors());
-
-
-router.post('/find', discountCodeController.findCode);
-module.exports = router;
--- server/routes/mail.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var mailController = require('../controllers/mail.controller');
-var cors = require('cors');
-router.options('/', cors());
-
-
-router.post('/sendSubscribeMail', mailController.sendSubscribeEmail);
-module.exports = router;
